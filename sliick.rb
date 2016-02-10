2cef2c30975c69d2d40203b87f62cceaa239d88e
Andrew Grathwohl <andrew@grathwohl.me>
2016-02-09 20:57:47 -0500

    init

---
+++ sliick.rb
@@ -0,0 +1,18 @@
+# sliick.rb
+require 'slack-ruby-bot'
+require 'cwiid'
+
+puts 'Put Wiimote in discoverable mode now (press 1+2)...'
+
+wiimote = WiiMote.new
+
+class PongBot < SlackRubyBot::Bot
+  command 'ping' do |client, data, match|
+    client.say(text: 'buzz', channel: data.channel)
+    wiimote.rumble = true
+    sleep 1
+    wiimote.rumble = false
+  end
+end
+
+PongBot.run
